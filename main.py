c126bc04baff94fa29a7a866068d6552b68d8946
krizsu <nowakk1629@gmail.com>
2026-07-02 20:55:51 +0200

    init discord bot

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+SERVER_ID = int(os.getenv("SERVER_ID"))
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    guild = discord.Object(SERVER_ID)
+    bot.tree.copy_global_to(guild=guild)
+    synced = await bot.tree.sync(guild=guild)
+    print(f"Synchronized {len(synced)} commands")
+
+async def main():
+    async with bot:
+        await bot.start(TOKEN)
+
+asyncio.run(main())
